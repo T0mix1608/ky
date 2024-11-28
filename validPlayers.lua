933a862af668980228249154cd5d0d66cbae7204
T0mix1608 <tomix1608.t@gmail.com>
2024-11-28 14:35:37 +0000

    Create validPlayers.lua

---
+++ validPlayers.lua
@@ -0,0 +1,11 @@
+return {
+    "Amethyst_Noob", -- owner
+    "1233XxTomixxX1233", -- owner
+    "robloxdestoryer12346", -- 34 nov.
+    "super_abobussik56", -- 27 dec.
+    "userealbaiterdotgg", -- life
+    "TheoTwo1234", -- 12 december
+    "rip_nonolh", -- 17th december 
+    "Jlab_roblox", -- 28 february 2025
+    "Herobrine49022" -- 1 month to never
+}
